748a5fb187b53fedf705df87bdb2b67e1dc8e0e4
Fabienne Prouteau <faabke@gmail.com>
2018-04-24 11:33:57 +0100

    added more pictures and caption to first page and added smooth transition

--- js/app.js
+++ js/app.js
@@ -1,3 +1,6 @@
+
+
+//NAVBAR
 // When the user scrolls down 20px from the top of the document, slide down the navbar
 // When the user scrolls to the top of the page, slide up the navbar (50px out of the top view)
 window.onscroll = function() {
@@ -12,6 +15,60 @@ function scrollFunction() {
   }
 }
 
+//Introduction Page
+//Change background picture and caption every 5 seconds.
+const pictArray = [{
+  link: '../assets/picture/coding.png',
+  caption: 'Web developer'
+},
+{
+  link: '../assets/picture/paris.jpg',
+  caption: 'Half-French'
+},
+{
+  link: '../assets/picture/amsterdam.jpg',
+  caption: 'Half-Dutch'
+},
+{
+  link: '../assets/picture/london.jpg',
+  caption: 'Londoner at heart'
+},
+{
+  link: '../assets/picture/animalLover.jpg',
+  caption: 'Nature & wildlife lover'
+}
+];
+
+//select the divs to set their backgroundImage and change text
+const destructuredPic = document.querySelectorAll('.destructuredPic');
+const caption = document.querySelector('.caption');
+
+//initialise backgroundImage and caption
+for (let i = 0; i < destructuredPic.length; i++) {
+  destructuredPic[i].style.backgroundImage = `url(${pictArray[0].link})`;
+}
+caption.innerText = pictArray[0].caption;
+caption.style.opacity = 1;
+
+//every 3 second update image and caption
+let picCaption = 0;
+setInterval(() => {
+  // change the background image for all divs
+  for (let i = 0; i < destructuredPic.length; i++) {
+    destructuredPic[i].style.backgroundImage = `url(${pictArray[picCaption].link})`;
+  }
+  // change caption text and opacity
+  caption.innerText = pictArray[picCaption].caption;
+  caption.style.opacity = 1;
+
+  picCaption < pictArray.length - 1 ? picCaption++ : picCaption = 0;
+}, 3000);
+
+
+
+
+
+//Project page
 //set click event to display more info on all projects
 const nbProject = 4;
 for (let i = 1 ; i <= nbProject; i++){
